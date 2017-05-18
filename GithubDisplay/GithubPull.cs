470781253bcd76686b1f926397b25864b5dab158
Keith-Ball <keith_ball@ymail.com>
2017-05-18 16:56:12 -0600

    Initial commit of the wpf app for showing that students have made a commit each day

---
+++ GithubDisplay/GithubPull.cs
@@ -0,0 +1,59 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubDisplay
+{
+    public class GithubPull : INotifyPropertyChanged
+    {
+        private ObservableCollection<Repository> orgRepos;
+        public ObservableCollection<Repository> OrgRepos {
+            get
+            {
+                return this.orgRepos;
+            }
+            set
+            {
+                this.orgRepos = value;
+                NotifyFieldChanged();
+            }
+        }
+
+        void NotifyFieldChanged([CallerMemberName] string fieldName = "")
+        {
+            var onPropertyChanged = PropertyChanged;
+            if (onPropertyChanged != null)
+            {
+                onPropertyChanged(this, new PropertyChangedEventArgs(fieldName));
+            }
+        }
+
+        public GithubPull()
+        {
+            PullGithubData();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async void PullGithubData()
+        {
+            var repos = await PullRepositories();
+            OrgRepos = new ObservableCollection<Repository>(repos);
+
+
+        }
+
+        private async Task<IEnumerable<Repository>> PullRepositories()
+        {
+            var githubClient = new GitHubClient(new ProductHeaderValue("PullingStudentsCommits"));
+            return await githubClient.Repository.GetAllForOrg("pro100-2017-Q2");
+            
+        }
+    }
+}
