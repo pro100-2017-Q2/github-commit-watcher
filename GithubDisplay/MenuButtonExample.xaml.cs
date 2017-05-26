e81ce59b26eaa98fa56c4d23a2d9f80abd3ffe14
Keith Ball <keith_ball@ymail.com>
2017-05-26 17:33:37 -0600

    Added a menu & menuitem example

---
+++ GithubDisplay/MenuButtonExample.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GithubDisplay
+{
+    /// <summary>
+    /// Interaction logic for MenuButtonExample.xaml
+    /// </summary>
+    public partial class MenuButtonExample : Window
+    {
+        public MenuButtonExample()
+        {
+            InitializeComponent();
+        }
+        
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as MenuItem).IsSubmenuOpen = true;
+        }
+
+        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as MenuItem).IsSubmenuOpen = false;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((sender as MenuItem).Header.ToString());
+        }
+    }
+}
